8473c4d72aea0e4673eac3a39a384a79d5a721fa
Rafael Gutiérrez <abadon.gutierrez@gmail.com>
2013-01-28 13:10:27 -0600

    Project's init state

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var shoppingApp = angular.module('shoppingApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+shoppingApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
